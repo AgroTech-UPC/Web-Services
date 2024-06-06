a6ee81a1b10e8bfb80c050583aa4dafdf9c561de
Andre Gabriel Valverde Mozo <u202218899@upc.edu.pe>
2024-06-05 20:42:16 -0500

    feat: Update CreateAvailableDateCommandFromEntityAssembler

--- src/main/java/com/acme/web/services/user/interfaces/rest/transform/CreateAvailableDateCommandFromEntityAssembler.java
+++ src/main/java/com/acme/web/services/user/interfaces/rest/transform/CreateAvailableDateCommandFromEntityAssembler.java
@@ -1,4 +1,15 @@
 package com.acme.web.services.user.interfaces.rest.transform;
 
+import com.acme.web.services.user.domain.model.commands.CreateAvailableDateCommand;
+import com.acme.web.services.user.interfaces.rest.resources.CreateAvailableDateResource;
+
 public class CreateAvailableDateCommandFromEntityAssembler {
+    public static CreateAvailableDateCommand toCommandFromResource(CreateAvailableDateResource resource){
+        return new CreateAvailableDateCommand(
+                resource.advisorId(),
+                resource.date(),
+                resource.startTime(),
+                resource.endTime()
+        );
+    }
 }
