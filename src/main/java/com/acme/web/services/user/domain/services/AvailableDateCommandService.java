32702d5bd65acc1e3da85db71409cc42beea8f92
Andre Gabriel Valverde Mozo <u202218899@upc.edu.pe>
2024-06-05 20:08:01 -0500

    feat: Update AvailableDateCommandService

--- src/main/java/com/acme/web/services/user/domain/services/AvailableDateCommandService.java
+++ src/main/java/com/acme/web/services/user/domain/services/AvailableDateCommandService.java
@@ -1,4 +1,9 @@
 package com.acme.web.services.user.domain.services;
 
+import com.acme.web.services.user.domain.model.commands.CreateAvailableDateCommand;
+import com.acme.web.services.user.domain.model.commands.DeleteAvailableDateCommand;
+
 public interface AvailableDateCommandService {
+    Long handle(CreateAvailableDateCommand command);
+    void handle(DeleteAvailableDateCommand command);
 }
