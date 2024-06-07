9c2dc72abda27aca41c1121b311dc5d0fdcc25c3
Piero Delgado <112528610+PieroD04@users.noreply.github.com>
2024-06-06 19:35:21 -0500

    feat: added querys for appointments by advisor or breeder id

--- src/main/java/com/acme/web/services/appointment/domain/services/AppointmentQueryService.java
+++ src/main/java/com/acme/web/services/appointment/domain/services/AppointmentQueryService.java
@@ -1,6 +1,8 @@
 package com.acme.web.services.appointment.domain.services;
 
 import com.acme.web.services.appointment.domain.model.aggregates.Appointment;
+import com.acme.web.services.appointment.domain.model.queries.GetAllAppointmentsByAdvisorIdQuery;
+import com.acme.web.services.appointment.domain.model.queries.GetAllAppointmentsByBreederIdQuery;
 import com.acme.web.services.appointment.domain.model.queries.GetAllAppointmentsQuery;
 import com.acme.web.services.appointment.domain.model.queries.GetAppointmentByIdQuery;
 
@@ -10,4 +12,6 @@ import java.util.Optional;
 public interface AppointmentQueryService {
     List<Appointment> handle(GetAllAppointmentsQuery query);
     Optional<Appointment> handle(GetAppointmentByIdQuery query);
+    List<Appointment> handle(GetAllAppointmentsByBreederIdQuery query);
+    List<Appointment> handle(GetAllAppointmentsByAdvisorIdQuery query);
 }
