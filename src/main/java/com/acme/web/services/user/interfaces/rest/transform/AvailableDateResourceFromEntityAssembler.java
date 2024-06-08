3a4cd158a8c85ee014b13d383b0aebc86feada78
Andre Gabriel Valverde Mozo <u202218899@upc.edu.pe>
2024-06-08 14:44:09 -0500

    fix: The status could not be changed

--- src/main/java/com/acme/web/services/user/interfaces/rest/transform/AvailableDateResourceFromEntityAssembler.java
+++ src/main/java/com/acme/web/services/user/interfaces/rest/transform/AvailableDateResourceFromEntityAssembler.java
@@ -11,7 +11,7 @@ public class AvailableDateResourceFromEntityAssembler {
                 entity.getDate(),
                 entity.getStartTime(),
                 entity.getEndTime(),
-                true
+                entity.getStatus()
         );
     }
 }
