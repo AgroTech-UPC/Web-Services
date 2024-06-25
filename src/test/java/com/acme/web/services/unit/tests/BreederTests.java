bc2baa12d05a4a2aa919c6de5c149a0301f0ba0d
sebastian123gonzalo <sebastian123gonzalo@gmail.com>
2024-06-24 20:47:10 -0500

    fix: BreederTests fixed

--- src/test/java/com/acme/web/services/unit/tests/BreederTests.java
+++ src/test/java/com/acme/web/services/unit/tests/BreederTests.java
@@ -1,14 +1,14 @@
 package com.acme.web.services.unit.tests;
 
 import com.acme.web.services.iam.domain.model.aggregates.User;
+import com.acme.web.services.user.domain.model.aggregates.Advisor;
 import com.acme.web.services.user.domain.model.aggregates.Breeder;
 import com.acme.web.services.user.domain.model.commands.CreateBreederCommand;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BreederTests {
 
@@ -31,9 +31,11 @@ public class BreederTests {
 
     @Test
     public void testBreederCreationWithNullUser() {
+        // Arrange
         CreateBreederCommand command = new CreateBreederCommand("fullname", "location", LocalDate.now().minusYears(30), "description", null);
-        assertThrows(IllegalArgumentException.class, () -> {
-            new Breeder(command, null);
-        });
+        // Act
+        Breeder breeder = new Breeder(command, null);
+        // Assert
+        assertNull(breeder.getUser());
     }
 }
